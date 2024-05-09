5345424f87c9524933fe854f66f9828a3bf45411
siamriaz <siam.riaz@selise.ch>
2024-05-10 04:26:24 +0600

    Events, Commands added

---
+++ MicroserviceWithRabbitMQ/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MessageType { get; protected set; } = string.Empty;
+        protected Message() 
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
